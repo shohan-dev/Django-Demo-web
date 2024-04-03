8928e3fe3607881eec82aada190732b120f10e29
Md Sabbir Roshid Shohan <59328735+shohan1010@users.noreply.github.com>
2024-04-04 01:42:42 +0600

    Pagination added and fix it to 5 num

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from shop.models import *
-
-
-# Create your views here.
+from django.core.paginator import Paginator
+from shop.models import Product
 
 def get_data(request):
-    data = Product.objects.all()
-    return render(request, 'product.html', {'data': data})
+    # Fetch all products
+    all_products = Product.objects.all()
+
+    # Number of items per page
+    items_per_page = 10
+
+    # Initialize the paginator with the fetched data and the number of items per page
+    paginator = Paginator(all_products, items_per_page)
+
+    # Get the current page number from the request, default to 1 if not provided
+    page_number = request.GET.get('page', 1)
+
+    # Get the products for the requested page
+    data = paginator.get_page(page_number)
+
+    return render(request, 'product.html', {'page_obj': data})
