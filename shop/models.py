c00bf77ff66213b8c5114fd38677b29fffe0d5ec
Md Sabbir Roshid Shohan <59328735+shohan1010@users.noreply.github.com>
2024-04-04 14:21:12 +0600

    added signal in product shop
    Signal handlers in Django, such as pre_save and post_save, are not directly meant for finding changes in the database. Instead, they are used to execute specific functions or tasks before or after certain actions are performed on the database, such as saving or deleting objects.

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -7,5 +9,16 @@ class Product(models.Model):
     price = models.IntegerField()
     image = models.ImageField()
     file = models.FileField()
+
     def __str__(self):
         return self.name
+    
+
+# signal handlers to find the changes in the database
+@receiver(pre_save, sender=Product)
+def print_status(sender, instance, **kwargs):
+    if instance.id:
+        old_name = sender.objects.get(id=instance.id).name
+        new_name = instance.name
+        print("Old Name:", old_name)
+        print("New Name:", new_name)
